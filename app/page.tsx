227b1ff3d282bde4488dc962185f1a455172dd99
Vitor Viana <vitorgovian@gmail.com>
2023-05-16 21:11:11 -0700

    add clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-"use client"
 import Image from "next/image"
+import { UserButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs"
 
 export default async function Home() {
+  const { userId } = auth()
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <UserButton />
+      <div>User Id: {userId}</div>
+    </main>
   )
 }
