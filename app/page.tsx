7b60184fff209bf7dd6f68252d38000107c23eaa
Vitor Viana <vitorgovian@gmail.com>
2023-05-18 07:26:22 -0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image"
 import { UserButton } from "@clerk/nextjs"
-import { auth } from "@clerk/nextjs"
+import { auth, currentUser } from "@clerk/nextjs"
+import { SignIn } from "@clerk/nextjs"
+import { SignInButton } from "@clerk/nextjs"
 
 export default async function Home() {
   const { userId } = auth()
+  const user = await currentUser()
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <UserButton />
-      <div>User Id: {userId}</div>
+      {userId ? (
+        <div>
+          <UserButton />
+          {userId}
+        </div>
+      ) : (
+        <SignInButton mode='modal' />
+      )}
     </main>
   )
 }
